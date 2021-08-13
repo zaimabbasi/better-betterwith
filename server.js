f758e3f58b754a5517c5039fc6dde61e4d0d6fc9
Zaim Abbasi <zaimabbasi@z170xpsli.Home>
2021-08-13 11:09:52 +0500

    remove get request method: excluded-ingredients

--- server.js
+++ server.js
@@ -61,30 +61,6 @@ app.get("/brands", (req, res) => {
     });
 });
 
-app.get("/excluded-ingredients", (req, res) => {
-    const excluded = [
-        "Canola Oil",
-        "Soy Oils",
-        "Potassium Sorbate",
-        "Sodium Citrate",
-        "Propylene Glycol Monostearate",
-        "Erythritol",
-        "Glycerin",
-        "Artificial Flavours",
-        "Artificial Colours",
-        "Modified Corn Starch",
-        "Carob Bean Gum",
-        "Locust Bean Gum",
-        "Sodium Benzoate",
-        "Cellulose Gum",
-        "Modified Milk Ingredients",
-        "Mono and Diglycerides",
-        "Modified Palm Oil"
-    ];
-
-    res.send(excluded);
-});
-
 app.get("/contacts", (req, res) => {
     Contact.find((err, contacts) => {
         if (err)    res.send(err);

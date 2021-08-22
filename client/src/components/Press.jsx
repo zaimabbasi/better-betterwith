9ea91069fc4acac4e2462869f79a885d7466c66e
Zaim Abbasi <zaimabbasi@z170xpsli.Home>
2021-08-22 12:18:08 +0500

    add press page

--- client/src/components/Press.jsx
+++ client/src/components/Press.jsx
@@ -1,11 +1,32 @@
 import React from "react";
-
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import BrandsData from "../sub-components/BrandsData";
 
 const Press = (props) => {
     return (
-        <section id="press">
-            This is Press sir.
-        </section>
+        <div id="press" className="section">
+            <Container>
+                <Row>
+                    {
+                        BrandsData.map(brandItem => {
+                            return (
+                                <Col md="4">
+                                    <img
+                                        className="w-100 mb-4"
+                                        src={brandItem.imgURL}
+                                        alt={brandItem.title}
+                                    />
+                                </Col>
+                            );
+                        })
+
+                    }
+
+                </Row>
+            </Container>
+        </div>
     );
 };
 
